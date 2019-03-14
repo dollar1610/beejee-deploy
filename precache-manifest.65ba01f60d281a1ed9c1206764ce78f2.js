fe566ab6aa03d5a0cdc625c0a16841ae7565e5c2
Виталий <Dollar16.10.91@mail.ru>
2019-03-14 11:17:48 +0500

    Updates

---
+++ precache-manifest.65ba01f60d281a1ed9c1206764ce78f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07db4759527574b06be1",
+    "url": "/beejee-deploy/static/js/runtime~main.5d7bd4ad.js"
+  },
+  {
+    "revision": "42aa60209289913d29c9",
+    "url": "/beejee-deploy/static/js/main.3373522e.chunk.js"
+  },
+  {
+    "revision": "f5c9edf3e9bc66ae3d6f",
+    "url": "/beejee-deploy/static/js/2.340a1934.chunk.js"
+  },
+  {
+    "revision": "42aa60209289913d29c9",
+    "url": "/beejee-deploy/static/css/main.34cdfff1.chunk.css"
+  },
+  {
+    "revision": "f5c9edf3e9bc66ae3d6f",
+    "url": "/beejee-deploy/static/css/2.dbc2d55c.chunk.css"
+  },
+  {
+    "revision": "b98bbcbc24ec4b2e8a739da5ab2b8d6f",
+    "url": "/beejee-deploy/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/beejee-deploy/precache-manifest.65ba01f60d281a1ed9c1206764ce78f2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/beejee-deploy/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
